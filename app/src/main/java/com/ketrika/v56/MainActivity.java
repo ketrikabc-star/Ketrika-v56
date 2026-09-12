1a65936d097c0c6ae4d305ede0fff52718524777
ketrikabc-star <ketrikabc@gmail.com>
2026-09-13 00:02:46 +0300

    Update and rename .github/workflows/build-apk.yml to app/src/main/java/com/ketrika/v56/MainActivity.java

---
+++ app/src/main/java/com/ketrika/v56/MainActivity.java
@@ -0,0 +1,9 @@
+package com.ketrika.v56;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

f22f4fc4cc88d20322e49e8090707829fabca9e8
ketrikabc-star <ketrikabc@gmail.com>
2026-09-13 10:28:28 +0300

    Update MainActivity.java

--- app/src/main/java/com/ketrika/v56/MainActivity.java
+++ app/src/main/java/com/ketrika/v56/MainActivity.java
@@ -1,9 +1,18 @@
 package com.ketrika.v56;
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
+import android.view.Gravity;
+
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("KETRIKA v56\nMAITSO! SUCCESS!");
+        tv.setTextSize(24);
+        tv.setGravity(Gravity.CENTER);
+        tv.setPadding(50, 200, 50, 50);
+        setContentView(tv);
     }
 }

d2fa4a1cd4b9a79767cff3ffb6e37d4b35fff1c2
ketrikabc-star <ketrikabc@gmail.com>
2026-09-13 08:18:07 +0300

    Delete MainActivity.kt

--- app/src/main/java/com/ketrika/v56/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.ketrika.v56
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
